750c0bf69e458c4e279e6e60ba5f2637b0ea78ef
mahsa khodaei <mahsa.khodaei811@gmail.com>
2024-04-20 17:28:46 +0330

    added font & card task & card filter

---
+++ js/todolist.js
@@ -0,0 +1,10 @@
+// const addTask = document.querySelector(".btn-add-task");
+// const cardTitle = document.querySelector(".title");
+// const cardTask = document.querySelector(".card-tasks");
+
+// addTask.addEventListener("click", () => {
+//   console.log("add task button");
+//   cardTitle.textContent = "Add task";
+//   addTask.textContent = "Add";
+//   document.querySelector("p").remove();
+// });
